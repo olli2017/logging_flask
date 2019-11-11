d1e3674ac7990bd824ba3df6d190bb1da34ce260
aydar <dusmukhambetov@mail.ru>
2019-11-11 19:10:04 +0300

    done logging

---
+++ http_server.py
@@ -0,0 +1,86 @@
+import redis
+from pymongo import MongoClient
+from flask import Flask, request
+from logging.config import dictConfig
+
+dictConfig({
+    'version': 1,
+    'formatters': {'default': {
+        'format': '[%(asctime)s] %(levelname)s in %(module)s: %(message)s',
+    }},
+    'handlers': {'wsgi': {
+        'class': 'logging.StreamHandler',
+        'stream': 'ext://flask.logging.wsgi_errors_stream',
+        'formatter': 'default'
+    }},
+    'root': {
+        'level': 'INFO',
+        'handlers': ['wsgi']
+    }
+})
+
+# from os import path
+# log_file_path = path.join(path.dirname(path.abspath(__file__)), 'log.config')
+# logging.config.fileConfig(log_file_path)
+
+app = Flask(__name__)
+
+cache = redis.Redis(host='redis', port=6379)
+
+db_client = MongoClient('mongodb://root:password@mongo')
+db = db_client.server_storage
+storage = db.storage
+
+@app.route('/put', methods=['POST', 'PUT'], endpoint="put")
+def put():
+    response = {}
+    storage.find_one_and_update(
+        {"key": request.values.get("key")}, {"$set": {"key": request.values.get("key"), "value": request.values.get("value")}}, upsert=True)
+    return response, 200
+
+
+@app.route('/get', methods=['GET'], endpoint="get")
+def get():
+    response = {
+        'from-cache': False
+    }
+    response_code = 200
+    cached_ans = None
+    if not request.values.get('no-cache'):
+        cached_ans = cache.get(request.values.get('key'))
+        app.logger.debug('get for key')
+
+    if request.values.get('no-cache') or not cached_ans:
+        app.logger.warning('no data in cache for key') # not in cache
+
+        storage_ans = storage.find_one({"key": request.values.get("key")})
+        app.logger.debug('get for key')
+        if storage_ans:
+           response["value"] = storage_ans['value']
+           response_code=200
+           if not request.values.get('no-cache'):
+               cache.set(request.values.get("key"), str(response['value']))
+        else:
+            app.logger.error('no data in database for key')
+            response_code = 404
+    else:
+        if type(cached_ans) is bytes:
+            cached_ans = cached_ans.decode('utf-8')
+        response['value'] = cached_ans
+        response['from-cache'] = True
+
+    return response, response_code
+
+
+@app.route('/delete', methods=['DELETE'], endpoint="delete")
+def get():
+    response = {}
+    response_code = 200
+    deleted_count = storage.delete_one({"key": request.values.get("key")}).deleted_count
+    if not deleted_count:
+        response_code = 404
+    return response, response_code
+
+
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0', port=65432)
